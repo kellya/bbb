66057bddec3d83b95878804f16d245efa5f5aff0
Alex Kelly <kellya@arachnitech.com>
2015-03-27 23:13:09 -0400

    Added path searching for 1-wire devices

--- temps.py
+++ temps.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import time
-w1="/sys/devices/w1_bus_master1/28-000005e719c4/w1_slave"
-w2="/sys/devices/w1_bus_master1/28-000005e5e1ec/w1_slave"
+import glob
 
-while True:
-	raw1=open(w1,"r").read()
-	raw2=open(w2,"r").read()
-	print "Temp1: "+str(round(float(raw1.split("t=")[-1])/1000,2)) +"°C"
-	print "Temp2: "+str(round(float(raw2.split("t=")[-1])/1000,2)) +"°C"
+devroot = "/sys/devices/w1_bus_master1"
+
+
+def ctof(rawval):
+	return str(((rawval*9)/5)+32)
+	
+tempint = []
+for tempdev in glob.glob(devroot+'/28-*/w1_slave'):
+	tempint.append(tempdev)
+
+while tempint > 0:
+	for device in tempint:
+		raw = open(device,"r").read()
+		print "Temp"+str(tempint.index(device))+": "+str(round(float(raw.split("t=")[-1])/1000,2)) +"°C"
 	print ""
 	time.sleep(2)
